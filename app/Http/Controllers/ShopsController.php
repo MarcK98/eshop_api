c41eb61c2f27f271c56794114d952f68bd579961
marckhoury <marc@mysayapp.com>
2020-12-20 20:57:05 +0200

    -MOD: getShopsWithCoordinates

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -9,11 +9,11 @@ class ShopsController extends Controller
 {
     public function getShopsWithCoordinates(){
         $shops = DB::table('shops')
-            ->selectRaw("`name`, `image`, `color`, GROUP_CONCAT(`lat`, ',', `long` SEPARATOR '; ') as locations")
+            ->selectRaw("`shops`.`id`,`name`, `image`, `color`, GROUP_CONCAT(`lat`, ',', `long` SEPARATOR '; ') as locations")
             ->leftJoin('locations', 'locations.shop_id', '=', 'shops.id')
             ->where('active', 1)
             ->whereNull('deleted_at')
-            ->groupBy('name', 'image', 'color')
+            ->groupBy('shops.id', 'name', 'image', 'color')
             ->get();
 
         return response()->json(['status' => true, 'shops' => $shops]);
